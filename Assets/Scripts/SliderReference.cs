341a27b538492457d351c84958cf0dc8cfa2927b
Ngak <ngakwastaken@gmail.com>
2019-05-15 10:47:29 -0500

    Initial commit

---
+++ Assets/Scripts/SliderReference.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderReference : MonoBehaviour
+{
+    public Slider input1, input2, output1, output2, output3;
+
+    public float maxWeight, maxDirt, maxWater, maxTime, maxSoap;
+
+    private void Start()
+    {
+        input1.maxValue = maxWeight;
+        input2.maxValue = maxDirt;
+        output1.maxValue = maxWater;
+        output2.maxValue = maxTime;
+        output3.maxValue = maxSoap;
+    }
+}
